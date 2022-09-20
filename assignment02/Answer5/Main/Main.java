519e054385e41f87d2e11f707a5cb23e4adba55b
Sharib Saifi <95537011+12Sharib@users.noreply.github.com>
2022-09-21 00:33:55 +0530

    Updated

---
+++ assignment02/Answer5/Main/Main.java
@@ -0,0 +1,26 @@
+package co.Assignment02.Answer5.Main;
+
+import co.Assignment02.Answer5.Operations.Operations;
+
+
+public class Main {
+	public static void main(String[] args) {
+		
+		String [] input = {"22, Rajan Anand, Engineering,1600000",
+				"23, Swati Patil, Testing,800000",
+				"27, Vijay Chawda, Engineering,800000",
+				"29, Basant Mahapatra, Engineering,600000",
+				"32, Ajay Patel, Testing,350000",
+				"34, Swaraj Birla,Testing,350000"};
+		
+		Operations op = new Operations();
+		
+		op.mapData(input);
+		
+		Integer maxSalary = op.maxSalary();
+		
+		op.printData(maxSalary);
+	
+
+	}
+}
